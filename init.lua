27dac6c25afc413f8fdf9768d23bd1a0f0853980
Ph-Dos <phm47860@gmail.com>
2025-10-19 13:42:03 -0500

    This the best music

--- init.lua
+++ init.lua
@@ -828,6 +828,7 @@ require('lazy').setup({
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
+
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
